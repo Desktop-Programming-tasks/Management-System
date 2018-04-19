d095a99721f35fcfbb18c1081a5e5d0c7e2f79fb
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-04-19 08:22:00 -0300

    validating add product

--- Project/src/main/modal/AddProductController.java
+++ Project/src/main/modal/AddProductController.java
@@ -9,7 +9,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import main.GUIController;
+import main.utils.Validate;
 
 /**
  * FXML Controller class
@@ -18,13 +20,33 @@ import main.GUIController;
  */
 public class AddProductController implements Initializable {
 
+    @FXML
+    private TextField ProductName;
+    @FXML
+    private TextField ProductPrice;
+    @FXML
+    private TextField ProductQuantity;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    @FXML
+    public void addProduct() {
+        if (!ProductName.getText().isEmpty() && !ProductPrice.getText().isEmpty()
+                && !ProductQuantity.getText().isEmpty()) {
+            try {
+                Integer.parseInt(ProductQuantity.getText());
+            } catch (Exception e) {
+                System.out.println("AddProduct::Erro");
+            }
+        }
+    }
+
     @FXML
     public void back() {
         GUIController.getInstance().closeModal();
