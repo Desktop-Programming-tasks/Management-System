a03122d774306fd23ab3d73134ad0462bfe51e95
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-04-19 15:07:44 -0300

    Menu

--- Project/src/main/IndexController.java
+++ Project/src/main/IndexController.java
@@ -30,79 +30,94 @@ public class IndexController implements Initializable {
 
     @FXML
     private void showMainAction(ActionEvent evt) {
-        Button btn = (Button)(evt.getSource());
+        Button btn = (Button) (evt.getSource());
         String actionType;
-        
-        if(btn.getText().equals("VENDA")) {
+
+        if (btn.getText().equals("VENDA")) {
             actionType = "Venda";
         } else {
             actionType = "Compra";
         }
-        
-        GUIController.getInstance().showMainActionScreen(actionType,false);
+
+        GUIController.getInstance().showMainActionScreen(actionType, false);
     }
-    
+    @FXML
+    private void showMainActionM(ActionEvent evt) {
+        MenuItem btn = (MenuItem) (evt.getSource());
+        String actionType;
+
+        if (btn.getText().equals("Registro de Vendas")) {
+            actionType = "Venda";
+        } else {
+            actionType = "Compra";
+        }
+
+        GUIController.getInstance().showMainActionScreen(actionType, false);
+    }
+
     @FXML
     private void showGenericTransaction(ActionEvent evt) {
         MenuItem itemName = (MenuItem) (evt.getSource());
-        
+
         GUIController.getInstance().showGenericTransationQuery(itemName.getText());
     }
-    
+
     @FXML
     private void showServiceQuery() {
         GUIController.getInstance().showServiceQuery();
     }
-    
+
     @FXML
     private void showBrandQuery() {
         GUIController.getInstance().showBrandQuery();
     }
-    
+
     @FXML
     private void showStockQuery() {
         GUIController.getInstance().showStockQuery();
     }
-    
+
     @FXML
     private void showPersonQuery(ActionEvent evt) {
         MenuItem itemName = (MenuItem) (evt.getSource());
-        
+
         GUIController.getInstance().showPersonQuery(itemName.getText());
     }
-    
-    @FXML 
+
+    @FXML
     private void showSupplierQuery() {
         GUIController.getInstance().showSupplierQuery();
     }
-    
+
     @FXML
     private void showCustomerRegister() {
         GUIController.getInstance().showCustomerRegister(false);
     }
-    
+
     @FXML
     private void showEmployeeRegister() {
         GUIController.getInstance().showEmployeeRegister(false);
     }
-    
+
     @FXML
     private void showProductRegister() {
         GUIController.getInstance().showProductRegister(false);
     }
-    
+
     @FXML
     private void showSupplierRegister() {
         GUIController.getInstance().showSupplierRegister(false);
     }
-    
+
     @FXML
     private void showModalRegisterBrand() {
         GUIController.getInstance().showModalRegisterBrand();
     }
-    
+
     @FXML
     private void showModalService() {
         GUIController.getInstance().showModalService();
     }
+
+
 }
