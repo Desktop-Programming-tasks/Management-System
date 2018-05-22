773ccbd4445747e944329c9b0ceef6b1fb7b2519
ecsanchesjr <ecsanchesjr@gmail.com>
2018-05-22 22:48:59 +0000

    Change package

--- DeskProject/src/desktoproject/Model/DAO/Transactions/ProductDAO.java
+++ DeskProject/src/desktoproject/Model/DAO/Transactions/ProductDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package desktoproject.Model.DAO.Persons.Transactions;
+package desktoproject.Model.DAO.Transactions;
 
 import desktoproject.Model.Classes.Transactions.Product;
 import java.util.ArrayList;
