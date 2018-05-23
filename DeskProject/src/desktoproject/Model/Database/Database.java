32cd6e95d2c45012ed3a6560189f2887bd569675
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-05-23 20:40:21 -0300

    Connection

--- DeskProject/src/desktoproject/Model/Database/Database.java
+++ DeskProject/src/desktoproject/Model/Database/Database.java
@@ -5,9 +5,13 @@
  */
 package desktoproject.Model.Database;
 
+import desktoproject.Utils.Pairs.QueryResult;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -18,13 +22,32 @@ public class Database {
     private Configs config;
     private final Connection connection;
 
-    ///////////////////////////////////////
-    public Configs getConfig() {
-        return config;
+    public void execute(String sql, Object... params) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(sql);
+        int i = 1;
+        for (Object o : params) {
+            statement.setObject(i++, o);
+        }
+        statement.executeUpdate();
     }
 
-    public void setConfig(Configs config) {
-        this.config = config;
+    public QueryResult query(String sql, Object... params) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement(sql);
+        int i = 0;
+        for (Object o : params) {
+            statement.setObject(i++, o);
+        }
+        ResultSet rs = statement.executeQuery();
+        return new QueryResult(rs, statement);
+    }
+
+    public QueryResult query(String sql) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(sql);
+        return new QueryResult(rs, statement);
+    }
+    public void disconnect() throws SQLException{
+        connection.close();
     }
 
     ////////////////////////////////////////
@@ -41,13 +64,22 @@ public class Database {
     public Database getInstance() throws SQLException, ClassNotFoundException {
         if (INSTANCE == null || connection == null) {
             INSTANCE = new Database();
-        }
-        else if (connection.isClosed()) {
+        } else if (connection.isClosed()) {
             INSTANCE = new Database();
         }
         return INSTANCE;
     }
 
     private Database INSTANCE;
+
+    ///////////////////////////////////////
+    ///////////////////////////////////////
+    public Configs getConfig() {
+        return config;
+    }
+
+    public void setConfig(Configs config) {
+        this.config = config;
+    }
     ///////////////////////////////////////
 }
--- DeskProject/src/desktoproject/Utils/Pairs/ControllerInfo.java
+++ DeskProject/src/desktoproject/Utils/Pairs/ControllerInfo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package desktoproject.Utils;
+package desktoproject.Utils.Pairs;
 
 import javafx.scene.Parent;
 
