50d0913bb14cf18f8c7e57649a2c86ed3d6c9e7b
ecsanchesjr <ecsanchesjr@gmail.com>
2018-05-24 17:53:59 +0000

    Database dumps and insert data

---
+++ Database/enums_database.sql
@@ -0,0 +1,10 @@
+INSERT INTO EmployeeType(nameEmployeeType) VALUES ('MANAGER');
+INSERT INTO EmployeeType(nameEmployeeType) VALUES ('COMMON');
+
+INSERT INTO ServiceStatus(nameServiceStatus) VALUES ('ON_ESTIMATE');
+INSERT INTO ServiceStatus(nameServiceStatus) VALUES ('WAITING_FOR_APPROVAL');
+INSERT INTO ServiceStatus(nameServiceStatus) VALUES ('WAITING_PAYMENT');
+INSERT INTO ServiceStatus(nameServiceStatus) VALUES ('WAITING_FOR_WITHDRAWAL');
+INSERT INTO ServiceStatus(nameServiceStatus) VALUES ('REFUSED');
+INSERT INTO ServiceStatus(nameServiceStatus) VALUES ('RUNNING');
+INSERT INTO ServiceStatus(nameServiceStatus) VALUES ('DONE');
