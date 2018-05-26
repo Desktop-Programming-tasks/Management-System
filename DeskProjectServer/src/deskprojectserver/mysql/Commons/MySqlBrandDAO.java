6e8b20e3cd6172ee2d0233f8672a5c1defffd409
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-05-26 00:35:33 -0300

    hold

---
+++ DeskProjectServer/src/deskprojectserver/mysql/Commons/MySqlBrandDAO.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deskprojectserver.mysql.Commons;
+
+import deskprojectserver.Classes.Transactions.Brand;
+import deskprojectserver.Database.DAO.Transactions.BrandDAO;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gabriel
+ */
+public class MySqlBrandDAO extends BrandDAO{
+
+    @Override
+    public void insertBrand(Brand brand) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateBrand(Brand brand) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeBrand(Brand brand) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Brand getBrand(String id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Brand> getAllBrands() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
