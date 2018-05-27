7799a1d997d0af4bc03771def6a8410636a5f05e
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-05-26 23:14:16 -0300

    more shits

--- DeskProjectServer/src/deskprojectserver/mysql/Commons/MySqlBrandDAO.java
+++ DeskProjectServer/src/deskprojectserver/mysql/Commons/MySqlBrandDAO.java
@@ -23,11 +23,8 @@ import java.util.ArrayList;
 public class MySqlBrandDAO extends BrandDAO {
 
     private static final String INSERT_SQL = "INSERT INTO `Brand`(`nameBrand`) VALUES (?)";
-    private static final String GET_ONE_SQL = "SELECT `idBrand`, `nameBrand` "
-            + "FROM `Brand` WHERE idBrand=?";
-    private static final String GET_ALL_SQL = "SELECT `idBrand`, `nameBrand` "
+    private static final String GET_ALL_SQL = "SELECT `nameBrand` "
             + "FROM `Brand` WHERE 1";
-    private static final String ID = "idBrand";
     private static final String NAME = "nameBrand";
 
     @Override
@@ -52,32 +49,12 @@ public class MySqlBrandDAO extends BrandDAO {
     }
 
     @Override
-    public Brand getBrand(String id) throws DatabaseErrorException, NoResultsException {
-        Brand brand = null;
-        try {
-            QueryResult qr = MySqlHandler.getInstance().getDb().query(GET_ONE_SQL, id);
-            while (qr.getResultSet().next()) {
-                brand = new Brand(qr.getResultSet().getInt(ID),
-                        qr.getResultSet().getString(NAME));
-            }
-            if (brand == null) {
-                throw new NoResultsException();
-            }
-            qr.closeAll();
-            return brand;
-        } catch (SQLException | ClassNotFoundException ex) {
-            throw new DatabaseErrorException();
-        }
-
-    }
-
-    @Override
     public ArrayList<Brand> getAllBrands() throws DatabaseErrorException {
         ArrayList<Brand> brands = new ArrayList<>();
         try {
             QueryResult qr = MySqlHandler.getInstance().getDb().query(GET_ALL_SQL);
             while (qr.getResultSet().next()) {
-                brands.add(new Brand(qr.getResultSet().getInt(ID), qr.getResultSet().getString(NAME)));
+                brands.add(new Brand(qr.getResultSet().getString(NAME)));
             }
             qr.closeAll();
         } catch (ClassNotFoundException | SQLException e) {
